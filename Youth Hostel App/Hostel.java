2547dd8ac79abae86e073d932a5b73f36b75d0ff
Ngoc Tram Nguyen <88424676+JKN22@users.noreply.github.com>
2022-08-08 14:39:08 -0700

    Create Hostel.java

---
+++ Youth Hostel App/Hostel.java
@@ -0,0 +1,15 @@
+package com.example.hostel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hostel extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hostel);
+    }
+}
