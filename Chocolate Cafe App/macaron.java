520d453a05af836f74ef9a04b2bceab1598c5d5e
Ngoc Tram Nguyen <88424676+JKN22@users.noreply.github.com>
2022-08-08 22:37:30 -0700

    Create macaron.java

---
+++ Chocolate Cafe App/macaron.java
@@ -0,0 +1,15 @@
+package com.example.chocolatecafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class macaron extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_macaron);
+    }
+}
