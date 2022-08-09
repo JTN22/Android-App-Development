5b211366b56291364cf731fe7af18cc8ec58495e
Ngoc Tram Nguyen <88424676+JKN22@users.noreply.github.com>
2022-08-08 22:31:31 -0700

    Create BeachBike.java

---
+++ Beach and Mountain Bike Rentals App/BeachBike.java
@@ -0,0 +1,15 @@
+package com.example.beachandmountainbikerental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeachBike extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beach_bike);
+    }
+}
